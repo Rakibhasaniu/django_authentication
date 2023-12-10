3158d98a2a68d1e39562d94a4d31b6edf14c3aaa
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-12-10 20:20:25 +0600

    form rendered in front-end

--- first_app/views.py
+++ first_app/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from .forms import RegisterForm
 # Create your views here.
 def home(request):
-    form = RegisterForm()
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save(commit = False)
+    else:
+        form = RegisterForm()
     return render(request,'home.html',{'data': form})
 
