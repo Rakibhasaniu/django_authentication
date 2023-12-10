2de5d85eb41e885392482189fc49a481608aedee
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-12-10 22:56:01 +0600

    created user

--- first_app/forms.py
+++ first_app/forms.py
@@ -3,9 +3,10 @@ from django.contrib.auth.forms import UserCreationForm
 from django import forms
 
 class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'id': 'required'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'id': 'required'}))   
     email = forms.CharField(required=True)
+
     class Meta:
-        first_name = forms.CharField(widget=forms.TextInput(attrs={'id': 'required'}))
-        last_name = forms.CharField(widget=forms.TextInput(attrs={'id': 'required'}))
         model = User
         fields =['username','first_name','last_name','email']
--- first_app/views.py
+++ first_app/views.py
@@ -5,7 +5,8 @@ def home(request):
     if request.method == 'POST':
         form = RegisterForm(request.POST)
         if form.is_valid():
-            form.save(commit = False)
+            form.save()
+            print(form.cleaned_data)
     else:
         form = RegisterForm()
     return render(request,'home.html',{'data': form})
