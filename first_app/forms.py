7c946b8ac8e1412469e0f31a153c2a0bd48314ef
Rakibhsaniu <rakibhasanoyoniu@gmail.com>
2023-12-10 21:17:44 +0600

    new features added

--- first_app/forms.py
+++ first_app/forms.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
 class RegisterForm(UserCreationForm):
+    email = forms.CharField(required=True)
     class Meta:
+        first_name = forms.CharField(widget=forms.TextInput(attrs={'id': 'required'}))
+        last_name = forms.CharField(widget=forms.TextInput(attrs={'id': 'required'}))
         model = User
         fields =['username','first_name','last_name','email']
